4ed93235313c03b6a9f0293463c2e895effa5986
chance-equalizer <chance.equalizer@protonmail.com>
2022-01-10 20:35:43 +0300

    ask for pic & name

--- bot.py
+++ bot.py
@@ -1,17 +1,55 @@
 import telebot
 import config
+from subprocess import Popen, PIPE
 
 bot = telebot.TeleBot(config.TOKEN)
 
+orders = dict()
+C = 1
+
+FILE_SIZE_LIMIT = 2 ** 20
+WAIT_MSG = "Please wait finish of the build process.."
+
+
+def unload_orders():
+    while orders:
+        order = orders.pop()
+
+
+def add_to_queue(name, picture):
+    args = ["scipts/build.sh", name, picture]
+    return Popen(args, shell=True, stdout=PIPE)
+
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    user_id = message.from_user.id
+    pic = message.photo[-1]
+    global C
+    C = 2
+    if not message.caption:
+        bot.send_message(user_id, f"Don't forget to add a caption")
+    elif pic.file_size > FILE_SIZE_LIMIT:
+        bot.send_message(user_id, f"File is too big, only 1Mb is allowed")
+    else:
+        bot.send_message(user_id, f"I see the photo of {message.caption}")
+        if user_id in orders:
+            msg = "Your prev order is replaced"
+        else:
+            msg = "Your order is recorded"
+
+        bot.send_message(user_id, f"{msg}. {WAIT_MSG}")
+
+        orders[user_id] = {'caption': message.caption, 'file_id': pic['file_id']}
+
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+def handle_text(message):
+    user_id = message.from_user.id
+    if user_id in orders:
+        bot.send_message(user_id, WAIT_MSG)
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, "Please attach pic with required name")
 
 
 bot.polling(none_stop=True, interval=0)
---
+++ scripts/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# build $1 $2
