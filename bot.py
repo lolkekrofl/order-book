b8ef8520e79372298e26ade46da877d0cce4d8d0
lsr <lsr@lsr.com>
2022-06-03 19:36:55 +0300

    Implement order configuration

--- bot.py
+++ bot.py
@@ -1,117 +1,78 @@
 import os
-
 import telebot
+
 import config
+from orders import OrdersQueue
 
 WAIT_MSG = "Please wait finish of the build process.."
 FILE_SIZE_LIMIT = 2 ** 20
-PAUSE = 10
-SCENARIO = [
-        'name',
-        'id',
-        'file',
-        'confirmation',
-        'waiting',
-        'backup'
-        ]  # name of the next step
 TEMP_DIR = config.TEMP_DIR
+DB_FILE = config.DB_FILE
 os.makedirs(TEMP_DIR, exist_ok=True)
 
-
-db = [] # todo: remove it
 bot = telebot.TeleBot(config.TOKEN)
-orders = dict()
+db = OrdersQueue(DB_FILE)
 
 
-def read_backup():
-    # todo: check database, read orders if possible
-    for record in db:
-        orders[record['user_id']] = order = dict()
-        order['name'] = record['name']
-        order['id'] = record['app_id']
-        order['file'] = record['icon_file']
-        order['stage'] = SCENARIO[5]
+@bot.message_handler(content_types=['text', 'document'])
+def handle_input(message):
 
+    userid = message.from_user.id
 
-def write_into_db(userid, name, app_id, icon_file):
-    pass # todo
-
-
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    time_to_build = False
-
-    user_id = message.from_user.id
-
-    if user_id not in orders:   # hello stage
-        orders[user_id] = {'stage': SCENARIO[0]}
-        bot.send_message(user_id, "Hello. Please give me name of app..")
-    else:
-        text = message.text
-        record = orders[user_id]
-        if record['stage'] == SCENARIO[0]:    # name
-            record['name'] = text
-            bot.send_message(user_id, "..id of app..")
-            record['stage'] = SCENARIO[1]
-        elif record['stage'] == SCENARIO[1]:  # id
-            record['id'] = text
-            bot.send_message(user_id, "..and attach icon file please")
-            record['stage'] = SCENARIO[2]
-        elif record['stage'] == SCENARIO[2]:    # icon
-            bot.send_message(user_id, "Wrong input")
-        elif record['stage'] == SCENARIO[3]:    # confirmation
-            if text.lower() in ('y', 'yes'):
-                bot.send_message(user_id, "Your order added to queue")
-
-                # only confirmation causes db update
-                write_into_db(user_id, record['name'], record['id'], record['file'])
-                time_to_build = True
-                record['stage'] = SCENARIO[4]   # waiting
-            else:
-                del orders[user_id]
-                bot.send_message(user_id, "Your order cancelled.")
-        # SCENARIO[4] case means just one more message from non-patient user, ignore it
-        elif record['stage'] == SCENARIO[5]:     # backup - recovery mode
-            time_to_build = True
-            record['stage'] = SCENARIO[4]  # waiting
-
-    if time_to_build:
-        pass
-        # todo:
-        # while queue_non_empty: wait
-        # call build script
-        # while build not ready: wait
-        # remove order
-        # from queue
-        # send build to user
-
-
-@bot.message_handler(content_types=['document'])
-def handle_document(message):
-    user_id = message.from_user.id
-    if user_id not in orders:
+    if userid not in db.get_users():   # hello stage
+        db.record_user(userid)
+        bot.send_message(userid, "Hello! Please give me name of the app")
         return
+    order = db.get_order(userid)
 
-    record = orders[user_id]
-    if record["stage"] != SCENARIO[2]:
-        return
-
-    document = message.document
-    if document.file_size > FILE_SIZE_LIMIT:
-        bot.send_message(user_id, f"File is too big, only 1Mb is allowed")
-    else:
+    if order.appname is None:
+        order.appname = message.text
+        bot.send_message(userid, f'App name is "{order.appname}".\n'
+                                 f'Now please provide the numeric ID for the app')
+    elif order.appid is None:
+        try:
+            appid = int(message.text)
+        except ValueError:
+            bot.send_message(userid, 'Please provide a numeric value for the app ID')
+        else:
+            order.appid = appid
+            bot.send_message(userid, f'App ID is {order.appid}.\n'
+                                     f'Please send the icon for the app')
+    elif order.appicon is None:
+        if message.document is None:
+            bot.send_message(userid, 'Please send the app icon as a document')
+            return
+        if message.document.file_size > FILE_SIZE_LIMIT:
+            bot.send_message(userid, f"Your file is too big."
+                                     f" Please send a file smaller than 1MB")
+            return
         # todo: download document.file_id and write it to disk
-        msg = f"Your order is recorded({record['name']}:{record['id']}).\nPlease confirm it (Yes/No)"
-        bot.send_message(user_id, msg)
-        record["stage"] = SCENARIO[3]
+        msg = f"Your order {order.appname}, ID={order.appname}" \
+              "\nPlease confirm it (yes/[no])"
+        bot.send_message(userid, msg)
+        order.appicon = bot.download_file(
+            bot.get_file(message.document.file_id).file_path
+        )
+    elif order.status is None:
+        if message.text.lower() not in ['y', 'yes']:
+            bot.send_message(userid, 'Your order is cancelled')
+            order.status = 'cancelled'
+            # TODO: remove cancelled and built orders in a separate async coroutine
+            db.remove_order(userid)
+        else:
+            bot.send_message(userid, 'Your order is confirmed and queued for building')
+            order.status = 'queued'
 
-        icon_file = bot.download_file(bot.get_file(document.file_id).file_path)
-        local_file_name = os.path.join(TEMP_DIR, '-'.join(("icon", str(user_id), document.file_name)))
+    elif order.status == 'queued':
+        bot.send_message(userid, f'Your order {order} is awaiting build.')
+        return
 
-        with open(local_file_name, "wb") as _file:
-            _file.write(icon_file)
-        record["file"] = local_file_name
+    db.update_order(userid,
+                    appname=order.appname,
+                    appid=order.appid,
+                    appicon=order.appicon,
+                    status=order.status)
 
 
-read_backup()
-bot.polling(none_stop=True, interval=0)
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
---
+++ orders.py
@@ -0,0 +1,104 @@
+import sqlite3
+
+
+class Order(object):
+    def __init__(self,
+                 userid: int,
+                 appname: str = None,
+                 appid: int = None,
+                 appicon: bytes = None,
+                 status: str = None):
+        self.userid = userid
+        self.appname = appname
+        self.appid = appid
+        self.appicon = appicon
+        self.status = status
+
+    def __repr__(self):
+        return f'Order(####{str(self.userid)[-3:]},' \
+               f' {self.appname}, {self.appid})'
+
+    def execute(self):
+        raise NotImplementedError
+
+
+class OrdersQueue(object):
+
+    def __init__(self, dbfile) -> None:
+        super().__init__()
+        self.dbfile = dbfile
+        try:
+            self.create_orders_table()
+        except sqlite3.Error:
+            pass
+
+    def init_connection(self):
+        return sqlite3.connect(self.dbfile)
+
+    def create_orders_table(self):
+        with self.init_connection() as con:
+            cur = con.cursor()
+            cur.execute("""create table orders (
+                    userid integer,
+                    appname text,
+                    appid integer,
+                    appicon blob,
+                    status text
+                    )""")
+        con.close()
+
+    def record_user(self, userid):
+        with self.init_connection() as con:
+            cur = con.cursor()
+            cur.execute("insert into orders (userid) values (?)", (userid, ))
+        con.close()
+
+    def update_order(self, userid,
+                     appname=None,
+                     appid=None,
+                     appicon=None,
+                     status=None):
+        if userid not in self.get_users():
+            raise ValueError(f"User #{userid} is not found")
+
+        with self.init_connection() as con:
+            cur = con.cursor()
+            cur.execute("""
+            update orders
+            set appname = :appname, appid = :appid, appicon = :appicon, status = :status
+            where userid = :userid
+            """, {
+                'userid': userid,
+                'appname': appname,
+                'appid': appid,
+                'appicon': appicon,
+                'status': status
+            })
+        con.close()
+
+    def remove_order(self, userid):
+        with self.init_connection() as con:
+            cur = con.cursor()
+            cur.execute("delete from orders where userid = ?", (userid,))
+        con.close()
+
+    def get_order(self, userid):
+        with self.init_connection() as con:
+            cur = con.cursor()
+            cur.execute('select * from orders where userid = ?', (userid,))
+            record = cur.fetchone()
+        con.close()
+        return Order(*record)
+
+    def get_orders(self):
+        for userid in self.get_users():
+            yield self.get_order(userid)
+
+    def get_users(self):
+        with self.init_connection() as con:
+            cur = con.cursor()
+            cur.execute('select userid from orders')
+            users = cur.fetchall()
+        con.close()
+        for user, in users:
+            yield user
