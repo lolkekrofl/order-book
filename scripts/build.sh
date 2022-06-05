3f6e723f87c1890b919078e2d38a4079142948f5
lsr <lsr@lsr.com>
2022-06-05 13:56:51 +0300

    Add dummy build script in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-# build $1 $2
+sleep 5
+mkdir -p "$1/TMessagesProj/build/outputs/apk/afat/release"
+echo "app id: $2, name: $3, icon: $4" > "$1/TMessagesProj/build/outputs/apk/afat/release/app.apk"
+touch "$1/done"
