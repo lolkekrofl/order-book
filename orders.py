b6758a45578968f0a1a1cc363400c723dedf5790
lsr <lsr@lsr.com>
2022-06-05 13:56:14 +0300

    Start implementing OrdersQueue.build_order(). Remove Order.execute().

--- orders.py
+++ orders.py
@@ -1,4 +1,6 @@
+import os.path
 import sqlite3
+import subprocess
 
 
 class Order(object):
@@ -17,10 +19,6 @@ class Order(object):
     def __repr__(self):
         return f'Order(####{str(self.userid)[-3:]},' \
                f' {self.appname}, {self.appid})'
-
-    def execute(self):
-        raise NotImplementedError
-
 
 class OrdersQueue(object):
 
@@ -104,3 +102,40 @@ class OrdersQueue(object):
         con.close()
         for user, in users:
             yield user
+
+    def build_order(self, order: Order, cmd: str, cwd: os.PathLike):
+        print(f'Starting build for userid {order.userid}')
+        iconfile = f'{order.userid}-{order.appid}-icon'
+        with open(os.path.join(cwd, iconfile), 'wb') as f:
+            f.write(order.appicon)
+        order.status = 'building'
+        self.update_order(order)
+        result = subprocess.run(
+            [cmd,
+             str(order.userid),
+             str(order.appid),
+             order.appname,
+             iconfile,
+             ], cwd=cwd)
+        if result == 0:
+            order.status = 'built'
+            print(f'Build for userid {order.userid} successful')
+        else:
+            order.status = 'build-failed'
+            print(f'Build for userid {order.userid} failed')
+        self.update_order(order)
+
+
+if __name__ == '__main__':
+    import config
+    test_db = os.path.join(config.TEMP_DIR, 'test.db')
+    if os.path.isfile(test_db):
+        os.remove(test_db)
+    db = OrdersQueue(test_db)
+    userid = 1
+    db.record_user(userid)
+    o = db.get_order(userid)
+    o.appid = 42
+    o.appname = 'TestApp'
+    db.update_order(o)
+    print(list(db.get_orders()))
